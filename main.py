514e3f72fd3aedb2c91edbd40e2817be57489af3
Daid <daid303@gmail.com>
2025-06-12 10:38:38 +0200

    update

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ client = discord.ext.commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.hybrid_command()
+@client.hybrid_command(description="Use this to give yourself the Rando Racer role, so you get pinged when there are races happening")
 async def irace(ctx):
     role = discord.utils.get(ctx.guild.roles, name='Rando Racer')
     await ctx.author.add_roles(role)
     await ctx.send('Your wish is my command.')
 
-@client.hybrid_command()
+@client.hybrid_command(description="Use this to take away the Rando Racer role")
 async def inorace(ctx):
     await ctx.author.remove_roles(discord.utils.get(ctx.guild.roles, name='Rando Racer'))
     await ctx.send('Your wish is my command.')
